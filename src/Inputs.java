9677c251e40b945db1819e9266a811c3766e6f11
Akshit900 <akshitkoundal91@gmail.com>
2025-08-18 23:06:18 +0530

    init commit

---
+++ src/Inputs.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Inputs {
+    public static void main(String[] args) {
+        //Integer Input from user
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter some Input: ");
+        int rollno = sc.nextInt();
+        System.out.println("Your roll number is " + rollno);
+        //String from user.
+//        String name = sc.nextLine();
+//        System.out.println();
+        //float from user.
+//        float marks = sc.nextFloat();
+//        System.out.println(marks);
+    }
+}
---
+++ src/Primitives.java
@@ -0,0 +1,14 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 1601;
+        char letter = 'r';
+        //All the decimal values are by default double so we have to add f at the last of float.
+        float marks = 98.67f;
+        double largeDecimalNumber = 980532.0011;
+        //used for larger values.
+        long largeInteger = 3438432566556678L;
+        boolean check = true;
+
+
+    }
+}
