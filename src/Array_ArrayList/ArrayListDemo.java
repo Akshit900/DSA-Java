a4ac2f686b140655f639dca436d31f68ae8020f9
Akshit900 <akshitkoundal91@gmail.com>
2025-11-05 23:47:48 +0530

    init commit

---
+++ src/Array_ArrayList/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package Array_ArrayList;
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        //Creating ArrayLiat
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        //Adding Elements in ArrayList
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        System.out.println(a);
+    }
+}
