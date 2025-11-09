6e07922f0b1502d30dac9e8b70d1282b2c07d811
Akshit900 <akshitkoundal91@gmail.com>
2025-11-09 16:48:28 +0530

    init commit

---
+++ src/Array_ArrayList/Question/MaxIteam.java
@@ -0,0 +1,18 @@
+package Array_ArrayList.Question;
+
+public class MaxIteam {
+    public static void main(String[] args) {
+        int [] arr = {1,3,29,8,18};
+        System.out.println(max(arr));
+    }
+    static int max (int [] arr){
+        int maxVal = arr[0];
+        for(int  i = 1; i < arr.length; i++){
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+
+            }
+        }
+        return maxVal;
+    }
+}
