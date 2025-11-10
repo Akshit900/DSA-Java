0f73418c171f20db03e98ddaf47d62a690598a3f
Akshit900 <akshitkoundal91@gmail.com>
2025-11-10 23:19:18 +0530

    init commit

---
+++ src/LinearSearch/Main.java
@@ -0,0 +1,27 @@
+package LinearSearch;
+
+public class Main {
+    public static int linearSearch(int[] arr, int key) {
+        // Loop through each element of the array
+        for (int i = 0; i < arr.length; i++) {
+            // If element is found, return its index
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        // If not found, return -1
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        int result = linearSearch(numbers, key);
+
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
+}
