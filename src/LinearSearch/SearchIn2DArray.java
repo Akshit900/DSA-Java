4bb91aca1c8185448a63fa3761efe77f75c4fc9a
Akshit900 <akshitkoundal91@gmail.com>
2025-11-12 00:28:01 +0530

    init commit

---
+++ src/LinearSearch/SearchIn2DArray.java
@@ -0,0 +1,28 @@
+package LinearSearch;
+
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int [][] arr = {
+                {2, 3, 4},
+                {18, 34, 56, 89},
+                {22, 44, 12, 32},
+                {55, 66, 77, 88, 99}
+        };
+        int target = 2;
+        int  [] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    static int [] search(int [] [] arr, int target){
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col < arr[row].length; col++){
+                if(arr[row] [col] == target){
+                    return new int[] {row, col};
+                }
+            }
+        }
+        return new int [] {-1, -1};
+    }
+}
