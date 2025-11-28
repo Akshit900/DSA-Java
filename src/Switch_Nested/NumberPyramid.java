dc059d547a11529525a8702e9a6adabbb5126a9e
Akshit900 <akshitkoundal91@gmail.com>
2025-11-28 21:28:59 +0530

    init commit

---
+++ src/Switch_Nested/NumberPyramid.java
@@ -0,0 +1,31 @@
+package Switch_Nested;
+
+import java.util.Scanner;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print leading spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+
+            // Increasing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            // Decreasing numbers
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
