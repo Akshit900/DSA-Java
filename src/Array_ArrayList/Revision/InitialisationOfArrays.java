ea2c6f33a1d50c1ac0f9e2f32a580396883e6668
Akshit900 <akshitkoundal91@gmail.com>
2026-02-05 00:27:56 +0530

    init commit

---
+++ src/Array_ArrayList/Revision/InitialisationOfArrays.java
@@ -0,0 +1,22 @@
+package Array_ArrayList.Revision;
+
+public class InitialisationOfArrays {
+    public static void main(String[] args) {
+//        int [] arr = {77,88,99,12,45};
+//        System.out.println(arr[3]);
+//        arr[3] = 66;
+//        System.out.println(arr[3]);
+
+        //Declare array
+//        int [] arr = new int[7];
+//        arr[3] = 20;
+//        System.out.println(arr[3]);
+
+        //How to print Array.
+        int [] arr = {76,88,12,99,100,36,45,66,98,99,76,55};
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
