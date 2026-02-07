53836acfab98694b336e535a19d015eda4566405
Akshit900 <akshitkoundal91@gmail.com>
2026-02-08 00:40:00 +0530

    init commit

---
+++ src/Array_ArrayList/Revision/TwoPointers.java
@@ -0,0 +1,35 @@
+package Array_ArrayList.Revision;
+
+public class TwoPointers {
+
+
+        public static int maxArea(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
+            int maxArea = 0;
+
+            while (left < right) {
+                int width = right - left;
+                int minHeight = Math.min(height[left], height[right]);
+                int area = width * minHeight;
+
+                maxArea = Math.max(maxArea, area);
+
+                if (height[left] < height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            return maxArea;
+        }
+
+        public static void main(String[] args) {
+            int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+            int result = maxArea(height);
+            System.out.println("Maximum Area = " + result);
+        }
+
+
+}
