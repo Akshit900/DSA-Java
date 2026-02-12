5801e853e88fa030bf815f004bf8c63d1e77fd4c
Akshit900 <akshitkoundal91@gmail.com>
2026-02-12 23:38:42 +0530

    init commit

--- src/StartingBasics/Main.java
+++ src/StartingBasics/Main.java
@@ -1,10 +1,14 @@
-package StartingBasics;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Scanner;
+package StartingBasics;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello India!");
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextLine());
+        String a = "John";
+        String b = " Doe";
+        String Sum = a+b;
+        System.out.println(Sum);
+
+        String name1 = new String("John");
+        String name2 = new String("John");
+        System.out.println(name1.equals(name2));
     }
 }
