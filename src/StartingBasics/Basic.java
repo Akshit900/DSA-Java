3dd9101a923b3e1c37eb158163464de6ab260022
Akshit900 <akshitkoundal91@gmail.com>
2026-02-13 00:24:54 +0530

    init commit

--- src/StartingBasics/Basic.java
+++ src/StartingBasics/Basic.java
@@ -1,10 +1,15 @@
 package StartingBasics;
 
+import java.util.Arrays;
+
 public class Basic {
     public static void main(String[] args) {
         String name = "John";
         System.out.println("Hello" + " " + name);
         System.out.println(name.charAt(0));
         System.out.println(name.concat(" " + "Doe"));
+
+        String name2 = "John Doe";
+        System.out.println(Arrays.toString(name2.toCharArray()));
     }
 }
