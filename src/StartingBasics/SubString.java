9a192b309372414b9de416be633f778a904e3eac
Akshit900 <akshitkoundal91@gmail.com>
2026-02-15 00:15:19 +0530

    init commit

---
+++ src/StartingBasics/SubString.java
@@ -0,0 +1,9 @@
+package StartingBasics;
+import java.util.*;
+public class SubString {
+    public static void main(String[] args) {
+        String str = "abcd";
+//        System.out.println(str.substring(0,4));
+        System.out.println(str.substring(0));
+    }
+}
