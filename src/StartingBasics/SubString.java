a6ea2559e71ee142ba11537dc3ca37830214db79
Akshit900 <akshitkoundal91@gmail.com>
2026-02-15 00:17:59 +0530

    init commit

--- src/StartingBasics/SubString.java
+++ src/StartingBasics/SubString.java
@@ -2,8 +2,13 @@ package StartingBasics;
 import java.util.*;
 public class SubString {
     public static void main(String[] args) {
-        String str = "abcd";
-//        System.out.println(str.substring(0,4));
-        System.out.println(str.substring(0));
+//        String str = "abcd";
+////        System.out.println(str.substring(0,4));
+//        System.out.println(str.substring(0));
+
+        String s ="johnDoe";
+        for(int j = 0; j < 4; j++){
+            System.out.println(s.substring(j));
+        }
     }
 }
