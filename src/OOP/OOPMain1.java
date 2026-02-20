689f2ba44972f539c1580357736989e7e54457e5
Akshit900 <akshitkoundal91@gmail.com>
2026-02-20 18:27:00 +0530

    init commit

--- src/OOP/OOPMain1.java
+++ src/OOP/OOPMain1.java
@@ -15,17 +15,30 @@ public class OOPMain1 {
         Student [] students = new Student[5];
 //        Student JohnDoe;
         Student JohnDoe = new Student();
-        JohnDoe.rollno = 13;
-        JohnDoe.name = "John Doe";
-        JohnDoe.marks = 99.0f;
+//        JohnDoe.rollno = 13;
+//        JohnDoe.name = "John Doe";
+//        JohnDoe.marks = 99.0f;
+        JohnDoe.greeting();
 
-        System.out.println(JohnDoe.rollno);
-        System.out.println(JohnDoe.name);
-        System.out.println(JohnDoe.marks);
+//        System.out.println(JohnDoe.rollno);
+//        System.out.println(JohnDoe.name);
+//        System.out.println(JohnDoe.marks);
     }
     static class Student{
         int rollno;
         String name;
         float marks;
+        //Constructor
+        //we need a way to add the value of the above properties object by object.
+        //we need one word to access every object.
+        void greeting()
+        {
+            System.out.println("Hello my name is " + this.name);
+        }
+        Student(){
+            this.rollno = 13;
+            this.name = "John Doe";
+            this.marks = 99.0f;
+        }
     }
 }
