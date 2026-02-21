de921163ba177dccefa494d7d7e4b2dbad5bee1d
Akshit900 <akshitkoundal91@gmail.com>
2026-02-21 23:26:07 +0530

    init commit

---
+++ src/OOP/Inheritance.java
@@ -0,0 +1,31 @@
+package OOP;
+// Parent class
+class Animal {
+
+    String name;
+
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+}
+
+// Child class
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println(name + " is barking");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+        Dog obj = new Dog();
+
+        obj.name = "Tommy";
+
+        obj.eat();   // inherited method
+        obj.bark();  // child method
+    }
+}
+
