f51e66c95053fafa36b5ca4b167f4963f63e2143
Akshit900 <akshitkoundal91@gmail.com>
2026-02-21 23:56:25 +0530

    init commit

---
+++ src/OOP/Inheritance/BoxMain.java
@@ -0,0 +1,8 @@
+package OOP.Inheritance;
+
+public class BoxMain {
+    public static void main(String[] args) {
+        Box box = new Box(10);
+        System.out.println(box.l+" "+box.w+" "+box.h);
+    }
+}
--- src/OOP/Inheritance/Inheritance.java
+++ src/OOP/Inheritance/Inheritance.java
@@ -1,4 +1,4 @@
-package OOP;
+package OOP.Inheritance;
 // Parent class
 class Animal {
 
