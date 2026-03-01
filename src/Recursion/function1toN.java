8548845db241a5393a072926bef012684f4fb0f5
Akshit900 <akshitkoundal91@gmail.com>
2026-03-01 23:26:08 +0530

    init commit

---
+++ src/Recursion/function1toN.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class function1toN {
+    public static void main(String[] args) {
+        funRev(5);
+    }
+    static void fun(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+    static void funRev(int n){
+        if(n==0){
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
+    }
+}
