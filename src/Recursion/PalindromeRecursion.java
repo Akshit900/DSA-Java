6c1fe989f678cca40d70fca1cc5a40cff99be5f8
Akshit900 <akshitkoundal91@gmail.com>
2026-03-03 22:28:08 +0530

    init commit

---
+++ src/Recursion/PalindromeRecursion.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class PalindromeRecursion {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+
+        boolean result = isPalindrome(str, 0, str.length() - 1);
+
+        if (result) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
