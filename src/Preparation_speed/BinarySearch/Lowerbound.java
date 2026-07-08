513178adda748f5c9fc2921319b894b55ccf1b64
Akshit900 <akshitkoundal91@gmail.com>
2026-07-09 01:04:19 +0530

    init commit

---
+++ src/Preparation_speed/BinarySearch/Lowerbound.java
@@ -0,0 +1,26 @@
+package Preparation_speed.BinarySearch;
+import java.util.*;
+public class Lowerbound {
+    public int lowerBound(int [] arr, int x){
+        int low = 0;
+        int high = arr.length-1;
+        int ans = arr.length;
+        while (low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int [] arr= {3, 5, 8, 15, 19};
+        int x = 9;
+        Lowerbound finder = new Lowerbound();
+        int ind = finder.lowerBound(arr, x);
+        System.out.println("The lower bound is the index: " + ind);
+    }
+}
